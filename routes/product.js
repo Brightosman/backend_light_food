8393e0a4b059bc6bea41b1902af5813b7b4858a4
Brightosman <brichiile@outlook.com>
2023-09-15 03:48:01 +0200

    2.3

--- routes/product.js
+++ routes/product.js
@@ -8,7 +8,7 @@ const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
 
 router.route('/products').get(getProducts);
 router.route('/admin/products').get(getAdminProducts);
-router.route('/product/:id').get(getSingleProduct);
+router.route('/products/:id').get(getSingleProduct);
 
 router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 router.route('/admin/product/:id')
