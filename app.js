150ed5e18333781a4ac8d0f786d642ade403f5ab
Brightosman <brichiile@outlook.com>
2023-09-20 23:30:19 +0200

    3.2

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 //const dotenv= require('dotenv');
 const path = require('path')
-const cors = require('cors') 
+//const cors = require('cors') 
 
 const errorMiddleware = require('./middlewares/errors')
 
 // setting config file 
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env'})
 
-app.use(cors())
+//app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
