f9baa73e3888a21e3077eff409fe64d6eb1f4c2e
Brightosman <brichiile@outlook.com>
2023-09-21 00:22:08 +0200

    3.6

--- routes/order.js
+++ routes/order.js
@@ -24,4 +24,4 @@ router.route('/admin/order/:id')
     .put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
 
-module.exports = router;<li>{data.seller}</li>
+module.exports = router;
